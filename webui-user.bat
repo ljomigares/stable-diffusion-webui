d2f4577dc2b84f027f0a81d79599696e1c22b93d
ljomigares <dick.holloway7@gmail.com>
2023-11-24 01:57:11 +0100

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --lowvram --precision full --no-half --skip-torch-cuda-test
+set COMMANDLINE_ARGS= --lowvram --precision full --no-half --skip-torch-cuda-test --skip-python-version-check
 
 call webui.bat
